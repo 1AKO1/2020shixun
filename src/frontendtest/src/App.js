40ef1eadcbb9486a4d5caee2b0a0cf7ab146cf13
aira <1418407007@qq.com>
2020-07-07 13:38:56 +0800

    mainPage小demo

--- src/frontendtest/src/App.js
+++ src/frontendtest/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Login from './pages/login/Login'
+import Register from "./pages/Register/Register";
+import Welcome from "./pages/Welcome/Welcome";
+import MainPage from "./pages/MainPage/MainPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Route path="/" exact component={Welcome}/>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register}/>
+          <Route path="/mainPage" component={MainPage}/>
+      </Router>
   );
 }
 
---
+++ src/frontendtest/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { Layout, Menu } from 'antd';
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+} from '@ant-design/icons';
+import './MainPage.css'
+import Logo from '../../logo.svg'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+const { SubMenu } = Menu;
+const { Header, Sider, Content, Footer } = Layout;
+
+export default class MainPage extends React.Component {
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo">
+                        <img src={Logo} alt="logo" style={{width: 75, height: 75, marginRight: 20}}/>
+                        {this.state.collapsed ? null : "React"}
+                    </div>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="main" icon={<UserOutlined />}>
+                            项目主页
+                        </Menu.Item>
+                        <SubMenu  key="DuanZ" icon={<VideoCameraOutlined />} title="段总">
+                            <Menu.Item key="10">数据表单</Menu.Item>
+                        </SubMenu>
+                        <SubMenu  key="LiuZ" icon={<UploadOutlined />}title="刘总">
+                            <Menu.Item key="20">数据表单</Menu.Item>
+                        </SubMenu>
+                        <SubMenu  key="LiaoZ" icon={<UploadOutlined />} title="廖总">
+                            <Menu.Item key="30">数据表单</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="XueZ" icon={<UploadOutlined />} title="薛总">
+                            <Menu.Item key="40">数据表单</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="LDD" icon={<UploadOutlined />} title="李弟弟">
+                            <Menu.Item key="50">数据表单</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0}}>
+                        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                            className: 'trigger',
+                            onClick: this.toggle,
+                        })}
+                    </Header>
+                    <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                    }}
+                >
+
+                </Content>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>@队友说的都队</Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+}
