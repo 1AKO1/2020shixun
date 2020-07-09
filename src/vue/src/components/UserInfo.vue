bfdb1b12e50a07e20b2ac7a4f69a5adfdb2cdd52
aira <1418407007@qq.com>
2020-07-09 10:36:21 +0800

    add UserInfo

--- src/vue/src/components/UserInfo.vue
+++ src/vue/src/components/UserInfo.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>右上角</div>
+    <a-dropdown>
+        <a-menu slot="overlay" @click="handleMenuClick">
+            <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item>
+            <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item>
+            <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> Button <a-icon type="down" /> </a-button>
+    </a-dropdown>
 </template>
 
 <script>
