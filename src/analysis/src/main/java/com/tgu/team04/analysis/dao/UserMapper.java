098576bb15cab087f0d828a4ab60775ee0e901f9
刘博 <a6824557@163.com>
2020-07-09 08:33:46 +0800

    用户sql语句

---
+++ src/analysis/src/main/java/com/tgu/team04/analysis/dao/UserMapper.java
@@ -0,0 +1,24 @@
+package com.tgu.team04.analysis.dao;
+
+import com.tgu.team04.analysis.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserMapper {
+
+    @Select("select * from user where uid = #{uid} and pwd = #{pwd}")
+    public User login(@Param("uid") String uid, @Param("pwd") String pwd);
+
+    @Select("select * from user where uid = #{uid}")
+    public  User selectByUid(@Param("uid") String uid);
+
+    @Insert("insert into user( uid,pwd,nick_name,email,address,sex,age) values(#{user.uid},#{user.pwd},#{user.nickName},#{user.email},#{user.address},#{user.sex},#{user.age})")
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    public int register(@Param("user") User user);
+}
+
