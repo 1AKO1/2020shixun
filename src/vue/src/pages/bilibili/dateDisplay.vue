675848e413c3ca36b87891ad748b80dabfabe4ed
aira <1418407007@qq.com>
2020-07-10 22:38:19 +0800

    简单图标完成 ，颜色有点丑

--- src/vue/src/pages/bilibili/dateDisplay.vue
+++ src/vue/src/pages/bilibili/dateDisplay.vue
@@ -5,15 +5,43 @@
                 title="评论分析"
                 sub-title="かぐや様は告らせたい〜天才たちの恋愛頭脳戦〜"
         />
+        <div class="easyBox">
+            <div class="easyItem">
+                <EchartsScore />
+            </div>
+            <div class="easyItem">
+                <EchartsProgress />
+            </div>
+            <div class="easyItem">
+                <EchartsVip />
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+    import EchartsScore from "../../components/bilibili/EchartsScore";
+    import EchartsProgress from "../../components/bilibili/EchartsProgress";
+    import EchartsVip from "../../components/bilibili/EchartsVip";
     export default {
-        name: "dateDisplay"
+        name: "dateDisplay",
+        components:{
+            EchartsScore,
+            EchartsProgress,
+            EchartsVip
+        }
     }
 </script>
 
 <style scoped>
-
+    .easyBox{
+        min-width: 1200px;
+        margin: 10px 0;
+        display: flex;
+        justify-content: space-between;
+    }
+    .easyItem{
+        border: 1px solid #001529;
+    }
 </style>
