ce276e68ba8fa986539eef10de638eb33b7944c4
刘博 <a6824557@163.com>
2020-07-10 18:46:29 +0800

    用户管理

--- src/analysis/src/main/java/com/tgu/team04/analysis/service/UserService.java
+++ src/analysis/src/main/java/com/tgu/team04/analysis/service/UserService.java
@@ -3,7 +3,17 @@ package com.tgu.team04.analysis.service;
 import com.tgu.team04.analysis.entity.TableData;
 import com.tgu.team04.analysis.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     TableData login(String uid, String pwd);
+
     TableData register(User user);
+
+    List<User> search(User user, int page, int limit);
+
+    boolean pwdReset(int id);
+
+    int searchCount(User user);
+
 }
