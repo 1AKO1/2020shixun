c3411544acc6acad8f13611f870bb5a1bc9fb169
Jiawei Liao <17674122259@163.com>
2020-07-11 22:40:32 +0800

    爬虫代码上传

--- other/Tesla/ScrapyPro/douban/douban/settings.py
+++ other/Tesla/ScrapyPro/douban/douban/settings.py
@@ -5,9 +5,9 @@
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
 #
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'douban'
 
@@ -21,30 +21,19 @@ MYSQL_USER = 'team04'
 MYSQL_PWD = 'team04'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+#USER_AGENT = 'douban (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-# PROXIES = [
-#     "119.27.177.238:8080",
-#     "49.4.67.31:3128",
-#     "218.60.8.99:3129",
-#     "116.196.85.150:3128",
-#     "139.217.110.76:3128",
-#     "222.186.36.210:3128",
-#     "112.253.11.113:8000",
-#     "27.191.234.69:9999"
-#     ]
-
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 2
-# RANDOMIZE_DOWNLOAD_DELAY = True
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -56,47 +45,43 @@ COOKIES_ENABLED = False
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-DEFAULT_REQUEST_HEADERS = {
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'en',
-#   'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
-}
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
 
 # Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
 #    'douban.middlewares.DoubanSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-   # 'douban.middlewares.ProxyMiddleware': 543,
-   'douban.middlewares.DoubanDownloaderMiddleware': 543,
-   'douban.middlewares.RotateUserAgentMiddleware': 542,
-   'douban.middlewares.IPProxyDownloadMiddleware': 100,
-   'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None
-   # 'douban.proxy_list.proxMiddleware': 300,
-   # 'douban.user_agent.RotateUserAgentMiddleware': 400
+    #'douban.middlewares.MyCustomDownloaderMiddleware': 543,
+    #'douban.proxy_list.proxMiddleware':300,
+'douban.user_agent.RotateUserAgentMiddleware':400
 }
 
 # Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 # Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'douban.pipelines.DoubanPipeline': 300,
-}
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+# ITEM_PIPELINES = {
+#     'douban.pipelines.DoubanPipeline': 300
+#
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+#AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
@@ -106,7 +91,7 @@ AUTOTHROTTLE_START_DELAY = 5
 #AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 #HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
---
+++ src/Tesla/douban/douban/spiders/book3_spider.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from douban.items import BookItem
+
+
+class BookSpider(CrawlSpider):
+    name = 'book3_spider'
+    allowed_domains = ['book.douban.com']
+    start_urls = ['https://book.douban.com/tag/']
+    rules = {
+        # div[3] 文化标签
+        Rule(LinkExtractor(allow='/tag/', restrict_xpaths="//div[@class='article']/div[2]/div[3]"), follow=True),
+        Rule(LinkExtractor(allow="\?start=\d+\&type=", restrict_xpaths="//div[@class='paginator']"), follow=True),
+        Rule(LinkExtractor(allow="/subject/\d+/$", restrict_xpaths="//ul[@class='subject-list']"), callback='parse_book')
+    }
+
+    def parse_book(self, response):
+        print("=" * 80)
+        print(response.url)
+        print("=" * 80)
+        item = BookItem()
+        # comm_item = BookHotCommItem()
+        if response.status == 200:
+            try:
+                item['book_url'] = response.url
+                item['book_picUrl'] = response.xpath('//div[@id="mainpic"]/a/@href').get()
+
+                item['book_title'] = response.xpath('//div[@id="wrapper"]/h1/span/text()').get()
+                item['book_author'] = get_author(response)
+                item['book_type'] = '文化'
+                item['book_press'] = response.xpath(u'//span[contains(./text(), "出版社:")]/following::text()[1]').get()
+                item['book_pubDate'] = get_pubdate(response)
+                item['book_tags'] = get_tags(response)
+                item['book_price'] = fix_field(response.xpath(u'//span[contains(./text(), "定价:")]/following::text()[1]').get())
+                item['book_isbn'] = response.xpath(u'//span[contains(./text(), "ISBN:")]/following::text()[1]').get()
+                item['book_report'] = get_report(response)
+                item['book_dbid'] = item['book_url'].split('/')[-2]
+                item['book_versions'] = get_versions(response)
+
+                item['book_score'] = get_score(response)
+                item['book_votes'] = convert_field(response.xpath(
+                    '//a[@class="rating_people"]/span/text()').get()) if response.xpath(
+                    '//a[@class="rating_people"]') else 0
+                item['book_shortComms'] = convert_field(response.xpath(
+                    '//div[@class="mod-hd"]/h2/span[2]/a/text()').get()).split(' ')[1] if response.xpath(
+                    '//div[@class="mod-hd"]/h2/span[2]') else 0
+                item['book_reviews'] = convert_field(response.xpath(
+                    '//section[@id="reviews-wrapper"]/header/h2/span/a/text()').get()).split(' ')[1]
+                item['book_fiveStar'] = float(convert_field(response.css(
+                    'span.rating_per')[0].css('::text').get()).split('%')[0]) / 100 if response.css(
+                    'span.rating_per') else 0
+                item['book_fourStar'] = float(convert_field(response.css(
+                    'span.rating_per')[1].css('::text').get()).split('%')[0]) / 100 if response.css(
+                    'span.rating_per') else 0
+                item['book_threeStar'] = float(convert_field(response.css(
+                    'span.rating_per')[2].css('::text').get()).split('%')[0]) / 100 if response.css(
+                    'span.rating_per') else 0
+                item['book_twoStar'] = float(convert_field(response.css(
+                    'span.rating_per')[3].css('::text').get()).split('%')[0]) / 100 if response.css(
+                    'span.rating_per') else 0
+                item['book_oneStar'] = float(convert_field(response.css(
+                    'span.rating_per')[4].css('::text').get()).split('%')[0]) / 100 if response.css(
+                    'span.rating_per') else 0
+
+                if response.xpath('//div[@id="collector"]/p[3]'):
+                    item['book_readings'] = int(convert_field(response.xpath(
+                        '//div[@id="collector"]/p[1]/a/text()').get()).split('人在读')[0])
+                    item['book_reads'] = int(convert_field(response.xpath(
+                        '//div[@id="collector"]/p[2]/a/text()').get()).split('人读过')[0])
+                    item['book_toReads'] = int(convert_field(response.xpath(
+                        '//div[@id="collector"]/p[3]/a/text()').get()).split('人想读')[0])
+
+                elif response.xpath('//div[@id="collector"]/p[3]') is None and response.xpath(
+                        '//div[@id="collector"]/p[2]'):
+                    item['book_readings'] = 0
+                    item['book_reads'] = int(convert_field(response.xpath(
+                        '//div[@id="collector"]/p[1]/a/text()').get()).split('人想读')[0])
+                    item['book_toReads'] = int(convert_field(response.xpath(
+                        '//div[@id="collector"]/p[2]/a/text()').get()).split('人想读')[0])
+
+                elif response.xpath('//div[@id="collector"]/p[2]') is None and response.xpath(
+                        '//div[@id="collector"]/p[1]'):
+                    item['book_readings'] = 0
+                    item['book_reads'] = 0
+                    item['book_toReads'] = int(convert_field(response.xpath(
+                        '//div[@id="collector"]/p[1]/a/text()').get()).split('人想读')[0])
+
+                else:
+                    item['book_readings'] = 0
+                    item['book_reads'] = 0
+                    item['book_toReads'] = 0
+
+                # 问题代码 不知道为什么会得到None
+                # hotComm = response.xpath('//div[@class="comment-list hot show"]/ul/li[1]/div')
+                # print('=' * 30 + '!' * 15 + '=' * 30)
+                # print(hotComm.xpath('/h3/span[1]'))
+                # print('=' * 30 + '!' * 15 + '=' * 30)
+                # comm_item['hotComm_people'] = hotComm.xpath(
+                #     '/h3/span[2]/a/text()').get()
+                # comm_item['hotComm_content'] = hotComm.xpath(
+                #     '/h3/p/span/text()').get()
+                # comm_item['hotComm_score'] = hotComm.xpath(
+                #     '/h3/span[2]/span[@class="user-stars"]/@title').get()
+                # comm_item['hotComm_votes'] = int(hotComm.xpath(
+                #     '/h3/span[1]/span/text()').get())
+
+                # item['hotComm_dbid'] = item['book_dbid']
+                item['hotComm_content'] = get_hotcommcontent(response)
+                item['hotComm_people'] = fix_field(response.xpath(
+                    '//div[@class="comment-list hot show"]/ul/li[1]/div/h3/span[2]/a/text()').get())
+                item['hotComm_score'] = fix_field(response.xpath(
+                    '//div[@class="comment-list hot show"]/ul/li[1]/div/h3/span[2]/span[@class="user-stars allstar50 rating"]/@title').get())
+                item['hotComm_votes'] = convert_field(response.xpath(
+                    '//div[@class="comment-list hot show"]/ul/li[1]/div/h3/span[1]/span/text()').get())
+
+                print('=' * 30 + '!' * 15 + '=' * 30)
+                yield item
+                print('=' * 30 + '!' * 15 + '=' * 30)
+                # yield comm_item
+                # print('=' * 30 + '!' * 15 + '=' * 30)
+
+            except Exception as error:
+                print('!' * 80)
+                print('【文化】分类爬虫爬取错误')
+                print('==> 错误书籍为：' + str(response.url))
+                print('==> 错误信息为：' + str(error))
+                print('!' * 80)
+
+        pass
+
+
+def convert_field(field):
+    return field.strip() if field else 0
+
+
+def fix_field(field):
+    return field.strip() if field else ''
+
+
+def get_author(response):
+    # 有些不同页面的author html会有所不同
+    author = response.css('#info > a:nth-child(2)::text').get()
+    if not author:
+        author = response.css('#info > span > a::text').get()
+    # 部分书籍如一千零一夜，没有作者，使用get，若无则直接返回None
+    return author.replace('\n            ', '').strip() if author else '无'
+
+
+def get_pubdate(response):
+    date = response.xpath(u'//span[contains(./text(), "出版年:")]/following::text()[1]').get()
+    if date:
+        return date.strip() if len(date.split('-')) > 2 else date.strip() + '-15'
+    else:
+        return '未知'
+
+
+def get_tags(response):
+    # 有问题，不知道怎么用xpath爬取tags
+    # tags_list = response.xpath('//*[@id="db-tags-section"]/div')
+    # tags = ''
+    # for tag in tags_list:
+    #     tags += '#'
+    #     tags += tag.xpath('/a/text()').get().strip()
+    #     tags += ' '
+    # return tags
+    tags_list = response.css('a.tag')
+    tags = ''
+    for tag in tags_list:
+        if tag:
+            tags += '#'
+            tags += tag.css('::text').get().strip()
+            tags += ' '
+    return tags if tags != '' else '无'
+
+
+def get_report(response):
+    report_list = response.css('#link-report > div:nth-child(1) > div > p::text').extract()
+    report = ''
+    for s in report_list:
+        report += s
+    return report
+
+
+def get_versions(response):
+    versions = response.css('div.version_works > h2 > span:nth-child(2) > a::text').get()
+    return int(versions.split('全部')[1]) if versions else 0
+
+
+def get_score(response):
+    score = response.xpath('//div[@class="rating_self clearfix"]/strong/text()').get().strip()
+    return float(score) if len(score) > 1 else 0.0
+
+
+def get_hotcommcontent(response):
+    content_list = response.xpath(
+        '//div[@class="comment-list hot show"]/ul/li[1]/div/p/span/text()').get()
+    content = ''
+    for s in content_list:
+        content += s
+    return content
