26345d5b4b78ad4ffb40b4487da9ceecd5a85e37
dcybdcy <1978360105@qq.com>
2020-07-12 22:33:38 +0800

    service层接口暂时完成

--- src/analysis/src/main/java/com/tgu/team04/analysis/service/impl/FilmServiceImpl.java
+++ src/analysis/src/main/java/com/tgu/team04/analysis/service/impl/FilmServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tgu.team04.analysis.service.impl;
 
 import com.tgu.team04.analysis.dao.FilmMapper;
+import com.tgu.team04.analysis.entity.AiraSimpleData;
 import com.tgu.team04.analysis.entity.FilmComment;
+import com.tgu.team04.analysis.entity.FilmSummeryData;
 import com.tgu.team04.analysis.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -47,13 +49,78 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public List<Float> maxScore() {
-        List<Float> maxmin = mapper.getMaxScore();
+//        List<Float> maxmin = mapper.getMaxScore();
 //        List<Float> list = new ArrayList<>();
 //        int maxscore=mapper.getPeopleNumber();
 //        int minscore=mapper.getTotalNumber();
 //        list.add(1,maxscore);
 //        list.add(2,minscore);
 //        Float maxscore = mapper.
-        return maxmin;
+//        return maxmin;
+        return  null;
+    }
+
+    @Override
+    public List<FilmComment> searchByOption(FilmComment filmComment, int page, int limit) {
+        if (filmComment.getMovieName() != null && !"".equals(filmComment.getMovieName().trim())){
+            filmComment.setMovieName("%" + filmComment.getMovieName() + "%");
+        }
+        if(filmComment.getMovieDirector() != null && !"".equals(filmComment.getMovieDirector().trim())){
+            filmComment.setMovieDirector("%" + filmComment.getMovieDirector() + "%");
+        }
+        if(filmComment.getMovieRole() != null && !"".equals(filmComment.getMovieRole().trim())){
+            filmComment.setMovieRole("%" + filmComment.getMovieRole() + "%");
+        }
+        if (page > 0 && limit > 0){
+            return mapper.selectByWhere(filmComment, (page - 1)*limit, limit);
+        }
+        return mapper.selectByWhere(filmComment, null, null);
+    }
+
+    @Override
+    public int searchCount(FilmComment filmComment) {
+        return mapper.countSelectByWhere(filmComment);
+    }
+
+    @Override
+    public List<FilmComment> simpleAnalysis(String type) {
+        List<FilmComment> comments = new ArrayList<>();
+        switch (type) {
+            case "score" :
+                comments = mapper.simpleAnalysis();
+                break;
+            case "commentRise" :
+                comments = mapper.commentAnalysis();
+                break;
+            case "wordFrequency" :
+                comments = mapper.areaAnalysis();
+                break;
+            case "fileCount" :
+                comments = mapper.filmCountAnalysis();
+                break;
+            case "timeScore" :
+                comments = mapper.timeScoreAnalysis();
+                break;
+
+        }
+
+        return comments;
+    }
+
+    @Override
+    public FilmSummeryData movieSummery() {
+        FilmSummeryData filmSummeryData  = new  FilmSummeryData();
+        int movieCount = mapper.getTotalNumber();
+        int movieLengh = mapper.getMovieLength();
+        float maxScore = mapper.getMaxScore();
+        float minScore = mapper.getMinScore();
+
+        int commentCount = mapper.getCommentCount();
+        filmSummeryData.setMovieMaxScore(maxScore);
+        filmSummeryData.setMovieMinScore(minScore);
+        filmSummeryData.setMovieCount(movieCount);
+        filmSummeryData.setMovieLength(movieLengh);
+        filmSummeryData.setMovieCommentCount(commentCount);
+        return filmSummeryData;
     }
 }
