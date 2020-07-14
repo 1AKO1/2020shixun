8d7af3fd48d3a8ef6d425397a61aa0458f3b3181
aira <1418407007@qq.com>
2020-07-14 13:33:14 +0800

    数据分析界面优化完成

--- src/vue/src/pages/bilibili/dateDisplay.vue
+++ src/vue/src/pages/bilibili/dateDisplay.vue
@@ -5,46 +5,115 @@
                 title="评论分析"
                 sub-title="かぐや様は告らせたい〜天才たちの恋愛頭脳戦〜"
         />
-<!--        第一层-->
-        <div class="easyBox">
-            <div class="easyItem">
-                <img src="../../static/kaguya.jpg" alt="" style="width: 400px; height: 400px; margin: 0 50px">
-            </div>
-            <div class="easyItem">
-                <EchartsGroupBySentiment />
-            </div>
-            <div class="easyItem">
-                <ScorePredict />
-            </div>
-<!--        第二层-->
-            <div class="easyItem">
-                <EchartsScore />
-            </div>
-            <div class="easyItem">
-                <EchartsProgress />
-            </div>
-            <div class="easyItem">
-                <EchartsVip />
-            </div>
-        </div>
-<!--        第三层-->
-        <div class="easyBox">
-            <div style="width: 100%">
-                <EchartsCommentRise />
-            </div>
-        </div>
-<!--        第四层-->
-        <div class="easyBox">
-            <div style="width: 100%">
-                <EchartsWordFrequency />
-            </div>
-        </div>
+        <br />
+        <a-row :gutter="16" style="margin-bottom: 10px">
+            <a-col :span="8">
+                <a-card hoverable>
+                    <a-statistic
+                            title="爬取数据总量"
+                            value="48235"
+                            :value-style="{ color: '#3f8600' }"
+                            style="margin-right: 50px"
+                    >
+                        <template #prefix>
+                            <a-icon type="arrow-up" />
+                        </template>
+                    </a-statistic>
+                </a-card>
+            </a-col>
 
-        <div class="easyBox">
-            <div style="width: 100%">
-                <EchartsSentiment />
-            </div>
-        </div>
+            <a-col :span="8">
+                <a-card hoverable>
+                    <a-statistic
+                            title="字段数量"
+                            :value="18"
+                            class="demo-class"
+                            :value-style="{ color: '#cf1322' }"
+                    >
+                        <template #prefix>
+                            <a-icon type="arrow-down" />
+                        </template>
+                    </a-statistic>
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card hoverable>
+                    <a-statistic
+                            title="番剧评分"
+                            :value="9.9"
+                            class="demo-class"
+                            :value-style="{ color: '#fb7299' }"
+                    >
+                        <template #prefix>
+                            <a-icon type="smile" />
+                        </template>
+                    </a-statistic>
+                </a-card>
+            </a-col>
+        </a-row>
+<!--        第一层-->
+
+        <a-row :gutter="16" style="margin-bottom: 10px">
+            <a-col :span="8">
+                <a-card title="评论词云" bordered hoverable>
+                    <img src="../../static/kaguya.jpg" alt="" style="width: 400px; height: 400px; margin: 0 50px">
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card title="用户评论情绪" bordered hoverable>
+                    <EchartsGroupBySentiment />
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <ScorePredict />
+            </a-col>
+
+        </a-row>
+
+        <a-row :gutter="16" style="margin-bottom: 10px">
+            <a-col :span="8">
+                <a-card title="评论各分段占比" bordered hoverable>
+                    <EchartsScore />
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card title="用户观看进度" bordered hoverable>
+                    <EchartsProgress />
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card title="用户付费情况" bordered hoverable>
+                    <EchartsVip />
+                </a-card>
+            </a-col>
+
+        </a-row>
+<!--        第三层-->
+
+        <a-row :gutter="16" style="margin-bottom: 10px">
+            <a-col :span="24">
+                <a-card title="每日评论数" bordered hoverable>
+                    <EchartsCommentRise />
+                </a-card>
+            </a-col>
+        </a-row>
+
+<!--        第四层-->
+        <a-row :gutter="16" style="margin-bottom: 10px">
+            <a-col :span="24">
+                <a-card title="动漫评论高频词TOP20" bordered hoverable>
+                    <EchartsWordFrequency />
+                </a-card>
+            </a-col>
+        </a-row>
+
+        <a-row :gutter="16" style="margin-bottom: 10px">
+            <a-col :span="24">
+                <a-card title="用户评论情绪与时间关系" bordered hoverable>
+                    <EchartsSentiment />
+                </a-card>
+            </a-col>
+        </a-row>
 
     </div>
 </template>
