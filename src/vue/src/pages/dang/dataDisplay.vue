98d2f978d6c0ec97e6db1b7685c4c4e446ebe759
zhihancn <9xueyanhan@163.com>
2020-07-14 08:57:05 +0800

    000

--- src/vue/src/pages/dang/dataDisplay.vue
+++ src/vue/src/pages/dang/dataDisplay.vue
@@ -1,32 +1,83 @@
 <template>
     <div>
-        <a-row>
+        <a-row :gutter="16">
+            <a-col :span="8">
+                <a-card hoverable>
+                    <a-statistic
+                            title="爬取总数"
+                            :value="42061"
+                            :value-style="{ color: '#cf1322' }"
+                            style="margin-right: 50px"
+                    >
+                        <template #prefix>
+                            <a-icon type="arrow-up" />
+                        </template>
+                    </a-statistic>
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card hoverable>
+                    <a-statistic
+                            title="平均推荐百分比"
+                            :value="98.37"
+                            :precision="2"
+                            suffix="%"
+                            class="demo-class"
+                            :value-style="{ color: '#3f8600' }"
+                    >
+                        <template #prefix>
+                            <a-icon type="arrow-down" />
+                        </template>
+                    </a-statistic>
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card hoverable>
+                    <a-statistic
+                            title="平均评论数"
+                            :value="400000"
+                            class="demo-class"
+                            :value-style="{ color: '#3f8600' }"
+                    >
+                        <template #prefix>
+                            <a-icon type="minus" />
+                        </template>
+                        <template #suffix>
+                            <span>/条</span>
+                        </template>
+
+                    </a-statistic>
+                </a-card>
+            </a-col>
+        </a-row>
+        <a-row :gutter="[16,16]">
             <a-col :span="12" style="width: 50%">
-                <a-card title="TOP图书分类" >
+                <a-card hoverable title="TOP图书分类" >
                     <EchartClasses/>
                 </a-card>
             </a-col>
             <a-col :span="12" style="width: 50%">
-                <a-card title="TOP图书出版信息">
+                <a-card hoverable title="TOP图书出版信息">
                     <EchartsProssTime/>
                 </a-card>
             </a-col>
         </a-row>
-        <a-row>
+        <a-row :gutter="[16,16]">
             <a-col :span="8" style="width: 50%">
-                <a-card title="TOP分类平均价格">
+                <a-card hoverable title="TOP分类平均价格">
                     <EchartsAvgClass/>
                 </a-card>
             </a-col>
             <a-col :span="8" style="width: 50%">
-                <a-card title="评论与推荐">
+                <a-card hoverable title="评论与推荐">
                     <EchartCommentPr/>
                 </a-card>
             </a-col>
+
         </a-row>
         <a-row>
-            <a-col :span="6" style="width: 50%">
-                <a-card title="4年TOP图书统计">
+            <a-col :span="8" style="width: 50%">
+                <a-card hoverable title="4年TOP图书统计">
                     <EchartyearBook/>
                 </a-card>
             </a-col>
