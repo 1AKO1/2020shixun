5634cf1192e63a3806375c17c1a1284f1ca54a64
Jiawei Liao <17674122259@163.com>
2020-07-15 08:58:28 +0800

    给typeScore分析增加默认项 默认只打开四项 让数据更清晰

--- src/vue/src/components/book/EchartsTypeScore.vue
+++ src/vue/src/components/book/EchartsTypeScore.vue
@@ -14,6 +14,7 @@
             return{
                 analysis_dataList:[],
                 analysis_type:[]
+
             }
         },
         methods: {
@@ -21,9 +22,14 @@
                 // 基于准备好的dom，初始化echarts实例
                 var myChart = this.$echarts.init(document.getElementById('typeScore'));
 
+                var selected_item ={
+                    '流行': false,
+                    '经管': false,
+                };
                 var option = {
                     tooltip: {},
                     legend: {
+                        selected: selected_item,
                         data: this.analysis_type
                     },
                     radar: {
